e23da42e577c3dff66830dc0f91af56760ef031e
Jordan Alexander Suwito <jordansuwito@gmail.com>
2025-11-27 11:43:51 +0700

    init commit

---
+++ app/src/main/java/tubes/DFS.java
@@ -0,0 +1,12 @@
+package tubes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DFS {
+    private List<Integer> visited;
+
+    public DFS() {
+        visited = new ArrayList<>();
+    }
+}
